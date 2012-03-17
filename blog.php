f2711008ae8d558fee5d2c442ccde948d12e2e85
Caitriona Raftery <caitriona.raftery@gmail.com>
2012-03-18 09:31:29 +1100

    update

--- blog.php
+++ blog.php
@@ -5,23 +5,28 @@
 <h1>Craftery Blog</h1>
 <p><a href="http://craftery-ie.tumblr.com/rss">RSS Feed</a></p>
 <hr/>
-http://api.tumblr.com/v2/blog/craftery-ie.tumblr.com/info?api_key=YCREdxJBoOW0IUVMVqGvz2oTVQR1pVhwrEcdrFyr2cylp7BMIp
 
 </div>
- <!--<script type="text/javascript" src="http://craftery-ie.tumblr.com/js"></script> 
-
-
--->
 <?php
 $request_url = 'http://craftery-ie.tumblr.com/api/read?type=post&start=0&num=20';
 $xml = simplexml_load_file($request_url);
+$blog_array = array();
+$blog_array = $xml->posts->post->{'regular-title'};
+$post_arry = array();
+$post_array = $xml->posts->post->{'regular-body'};
+$count = count($blog_array);
+$index = 1;
+print_r($blog_array);
+print_r($post_array);
+for ($i = $index; $i < $count; $i++) {
+	$j = $blog_array[$i];
+}
 $title = $xml->posts->post->{'regular-title'};
 $post = $xml->posts->post->{'regular-body'};
 $link = $xml->posts->post['url'];
 $small_post = substr($post,0,320);
-echo '<h1>'.$title.'</h1>';
-echo '<p>'.$post.'</p>';
-echo '…'; 
 ?>
+<!-- echo '<h1>'.$title.'</h1>';
+echo '<p>'.$post.'</p>'; -->
 </div>
 <?php  include "footer.php" ?>

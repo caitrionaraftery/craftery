f79f6fd0691473b4e2e84b353f9da2a580007d01
Triona Raftery <caitriona.raftery@gmail.com>
2012-04-03 15:57:45 +1000

    changing layout of blog page

--- blog.php
+++ blog.php
@@ -4,31 +4,30 @@
 	<div align="center">
 <h1>Craftery Blog <a href="http://craftery-ie.tumblr.com/rss"><img src="images/icons/default/16/rss.png"></a></h1>
 
-<hr/>
+
 
 </div>
+
 <?php
 $request_url = 'http://craftery-ie.tumblr.com/api/read?type=post&start=0&num=20';
 $xml = simplexml_load_file($request_url);
-print_r($xml);
-
-
-
-$small_post = substr($xml,0,320);
-echo $small_post;
-
-
 $count = count($xml);
+echo $count;
 echo '<br>';
 for ($i = 0; $i < $count; $i++) {
+?>
+<div class="hero-unit">
+<?php
 	$title = $xml->posts->post[$i]->{'regular-title'};
 	$date =  $xml->posts->post[$i]->attributes()->date;
 	echo '<h2>'.$title.'</h2>';
 	echo '<h4>'.$date.'</h4>';
+?>
+</div>
+<?php
 }
 
 
 ?>
-
 
 <?php  include "footer.php" ?>

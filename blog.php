2a9ac0d6e36687faa68573446ed5c82e4f4b04e7
Triona Raftery <caitriona.raftery@gmail.com>
2012-04-03 14:35:34 +1000

    update to blog, correctly printing out information

--- blog.php
+++ blog.php
@@ -10,23 +10,25 @@
 <?php
 $request_url = 'http://craftery-ie.tumblr.com/api/read?type=post&start=0&num=20';
 $xml = simplexml_load_file($request_url);
-$blog_array = array();
-$blog_array = $xml->posts->post->{'regular-title'};
-$post_arry = array();
-$post_array = $xml->posts->post->{'regular-body'};
-$count = count($blog_array);
-$index = 1;
-print_r($blog_array);
-print_r($post_array);
-for ($i = $index; $i < $count; $i++) {
-	$j = $blog_array[$i];
+print_r($xml);
+
+
+
+$small_post = substr($xml,0,320);
+echo $small_post;
+
+
+$count = count($xml);
+echo '<br>';
+for ($i = 0; $i < $count; $i++) {
+	$title = $xml->posts->post[$i]->{'regular-title'};
+	$date =  $xml->posts->post[$i]->attributes()->date;
+	echo '<h2>'.$title.'</h2>';
+	echo '<h4>'.$date.'</h4>';
 }
-$title = $xml->posts->post->{'regular-title'};
-$post = $xml->posts->post->{'regular-body'};
-$link = $xml->posts->post['url'];
-$small_post = substr($post,0,320);
+
+
 ?>
-<!-- echo '<h1>'.$title.'</h1>';
-echo '<p>'.$post.'</p>'; -->
+
 
 <?php  include "footer.php" ?>

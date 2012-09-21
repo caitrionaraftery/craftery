943d1a82646b7a3910e7058afe7a3d1c9cdafd71
caitrionaraftery <caitriona.raftery@gmail.com>
2012-09-21 14:15:49 +0100

    refactoring

---
+++ statcounter.php
@@ -0,0 +1,16 @@
+
+<!-- Start of StatCounter Code -->
+
+<script type="text/javascript"><!--
+
+var sc_project=6507585; 
+
+var sc_invisible=1; 
+
+var sc_security="d1c59512";
+
+// --></script>
+
+<script src="http://www.statcounter.com/counter/counter.js" type="text/javascript"></script>
+
+<noscript>&amp;lt;div class="statcounter"&amp;gt;&amp;lt;a title="godaddy web statistics" href="http://statcounter.com/godaddy_website_tonight/" target="_blank"&amp;gt;&amp;lt;img class="statcounter" src="http://c.statcounter.com/6507585/0/d1c59512/1/" alt="godaddy web statistics" &amp;gt;&amp;lt;/a&amp;gt;&amp;lt;/div&amp;gt;</noscript> <!-- End of StatCounter Code --></div>

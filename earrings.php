af0612d5cc8508b919723d836f6b454957753991
caitrionaraftery <caitriona.raftery@gmail.com>
2012-09-30 00:31:46 +0100

    update to earrings

--- earrings.php
+++ earrings.php
@@ -6,6 +6,350 @@
 	<?php  include "sales-info.php" ?>
 
 <ul class="thumbnails">
+	
+<li class="span6">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/neckear/2-19-IMG_20120929_195140.jpg" class="thumbnail" title="item: e-093">
+		  	<img src="images/collections/earrings/neckear/2-19-IMG_20120929_195140.jpg"></a>
+			<h5>item: e-093</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span6">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/neckear/5-22-IMG_20120929_195448.jpg" class="thumbnail" title="item: e-092">
+		  	<img src="images/collections/earrings/neckear/5-22-IMG_20120929_195448.jpg"></a>
+			<h5>item: e-092</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/neckear/4-21-IMG_20120929_195415.jpg" class="thumbnail" title="item: e-091">
+		  	<img src="images/collections/earrings/neckear/4-21-IMG_20120929_195415.jpg"></a>
+			<h5>item: e-091</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/neckear/3-20-IMG_20120929_195329.jpg" class="thumbnail" title="item: e-090">
+		  	<img src="images/collections/earrings/neckear/3-20-IMG_20120929_195329.jpg"></a>
+			<h5>item: e-090</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/neckear/1-23-IMG_20120929_195527.jpg" class="thumbnail" title="item: e-089">
+		  	<img src="images/collections/earrings/neckear/1-23-IMG_20120929_195527.jpg"></a>
+			<h5>item: e-089</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/12-glass 29-09-2012 20-38-11.jpg" class="thumbnail" title="item: e-088">
+		  	<img src="images/collections/earrings/glass/12-glass 29-09-2012 20-38-11.jpg"></a>
+			<h5>item: e-088</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/11-glass 29-09-2012 20-37-33.jpg" class="thumbnail" title="item: e-087">
+		  	<img src="images/collections/earrings/glass/11-glass 29-09-2012 20-37-33.jpg"></a>
+			<h5>item: e-087</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/10-glass 29-09-2012 20-41-19.jpg" class="thumbnail" title="item: e-086">
+		  	<img src="images/collections/earrings/glass/10-glass 29-09-2012 20-41-19.jpg"></a>
+			<h5>item: e-086</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/09-glass 29-09-2012 20-39-36.jpg" class="thumbnail" title="item: e-085">
+		  	<img src="images/collections/earrings/glass/09-glass 29-09-2012 20-39-36.jpg"></a>
+			<h5>item: e-085</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/08-glass 29-09-2012 20-16-23.jpg" class="thumbnail" title="item: e-084">
+		  	<img src="images/collections/earrings/glass/08-glass 29-09-2012 20-16-23.jpg"></a>
+			<h5>item: e-084</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/07-glass 29-09-2012 20-16-55.jpg" class="thumbnail" title="item: e-083">
+		  	<img src="images/collections/earrings/glass/07-glass 29-09-2012 20-16-55.jpg"></a>
+			<h5>item: e-083</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+		</ul>
+	<ul class="thumbnails">
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/06-glass 29-09-2012 20-19-17.jpg" class="thumbnail" title="item: e-082">
+		  	<img src="images/collections/earrings/glass/06-glass 29-09-2012 20-19-17.jpg"></a>
+			<h5>item: e-082</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/05-glass 29-09-2012 20-19-54.jpg" class="thumbnail" title="item: e-081">
+		  	<img src="images/collections/earrings/glass/05-glass 29-09-2012 20-19-54.jpg"></a>
+			<h5>item: e-081</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/04-glass 29-09-2012 20-21-03.jpg" class="thumbnail" title="item: e-080">
+		  	<img src="images/collections/earrings/glass/04-glass 29-09-2012 20-21-03.jpg"></a>
+			<h5>item: e-080</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/03-glass 29-09-2012 20-22-02.jpg" class="thumbnail" title="item: e-079">
+		  	<img src="images/collections/earrings/glass/03-glass 29-09-2012 20-22-02.jpg"></a>
+			<h5>item: e-079</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/02-glass 29-09-2012 20-23-19.jpg" class="thumbnail" title="item: e-078">
+		  	<img src="images/collections/earrings/glass/02-glass 29-09-2012 20-23-19.jpg"></a>
+			<h5>item: e-078</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/glass/01-glass 29-09-2012 20-24-59.jpg" class="thumbnail" title="item: e-077">
+		  	<img src="images/collections/earrings/glass/01-glass 29-09-2012 20-24-59.jpg"></a>
+			<h5>item: e-077</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/6-36-IMG_20120929_202710.jpg" class="thumbnail" title="item: e-076">
+		  	<img src="images/collections/earrings/cateye/6-36-IMG_20120929_202710.jpg"></a>
+			<h5>item: e-076</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/5-35-IMG_20120929_202815.jpg" class="thumbnail" title="item: e-075">
+		  	<img src="images/collections/earrings/cateye/5-35-IMG_20120929_202815.jpg"></a>
+			<h5>item: e-075</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/4-34-IMG_20120929_202942.jpg" class="thumbnail" title="item: e-074">
+		  	<img src="images/collections/earrings/cateye/4-34-IMG_20120929_202942.jpg"></a>
+			<h5>item: e-074</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/3-33-IMG_20120929_203146.jpg" class="thumbnail" title="item: e-073">
+		  	<img src="images/collections/earrings/cateye/3-33-IMG_20120929_203146.jpg"></a>
+			<h5>item: e-073</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/2-32-IMG_20120929_203243.jpg" class="thumbnail" title="item: e-072">
+		  	<img src="images/collections/earrings/cateye/2-32-IMG_20120929_203243.jpg"></a>
+			<h5>item: e-072</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/cateye/1-37-IMG_20120929_203330.jpg" class="thumbnail" title="item: e-071">
+		  	<img src="images/collections/earrings/cateye/1-37-IMG_20120929_203330.jpg"></a>
+			<h5>item: e-071</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/49-IMG_20120929_205609.jpg" class="thumbnail" title="item: e-070">
+		  	<img src="images/collections/earrings/pearl/49-IMG_20120929_205609.jpg"></a>
+			<h5>item: e-070</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/48-IMG_20120929_205438.jpg" class="thumbnail" title="item: e-069">
+		  	<img src="images/collections/earrings/pearl/48-IMG_20120929_205438.jpg"></a>
+			<h5>item: e-069</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/47-IMG_20120929_205254.jpg" class="thumbnail" title="item: e-068">
+		  	<img src="images/collections/earrings/pearl/47-IMG_20120929_205254.jpg"></a>
+			<h5>item: e-068</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/46-IMG_20120929_205206.jpg" class="thumbnail" title="item: e-067">
+		  	<img src="images/collections/earrings/pearl/46-IMG_20120929_205206.jpg"></a>
+			<h5>item: e-067</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/45-IMG_20120929_205014.jpg" class="thumbnail" title="item: e-066">
+		  	<img src="images/collections/earrings/pearl/45-IMG_20120929_205014.jpg"></a>
+			<h5>item: e-066</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/44-IMG_20120929_204932.jpg" class="thumbnail" title="item: e-065">
+		  	<img src="images/collections/earrings/pearl/44-IMG_20120929_204932.jpg"></a>
+			<h5>item: e-065</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/43-IMG_20120929_204833.jpg" class="thumbnail" title="item: e-064">
+		  	<img src="images/collections/earrings/pearl/43-IMG_20120929_204833.jpg"></a>
+			<h5>item: e-064</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/42-IMG_20120929_204735.jpg" class="thumbnail" title="item: e-063">
+		  	<img src="images/collections/earrings/pearl/42-IMG_20120929_204735.jpg"></a>
+			<h5>item: e-063</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/41-IMG_20120929_204659.jpg" class="thumbnail" title="item: e-062">
+		  	<img src="images/collections/earrings/pearl/41-IMG_20120929_204659.jpg"></a>
+			<h5>item: e-062</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	</ul>
+<ul class="thumbnails">
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/40-IMG_20120929_204615.jpg" class="thumbnail" title="item: e-061">
+		  	<img src="images/collections/earrings/pearl/40-IMG_20120929_204615.jpg"></a>
+			<h5>item: e-061</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/39-IMG_20120929_204539.jpg" class="thumbnail" title="item: e-060">
+		  	<img src="images/collections/earrings/pearl/39-IMG_20120929_204539.jpg"></a>
+			<h5>item: e-060</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+	<li class="span4">
+		<div class="thumbnail">
+		  <a href="images/collections/earrings/pearl/38-IMG_20120929_205634.jpg" class="thumbnail" title="item: e-059">
+		  	<img src="images/collections/earrings/pearl/38-IMG_20120929_205634.jpg"></a>
+			<h5>item: e-059</h5>
+			<!-- <p>Thumbnail caption right here...</p> -->
+		</div>
+	</li>
+	
+</ul>
+<ul class="thumbnails">
+	
+	
 	<li class="span4">
 		<div class="thumbnail">
 		  <a href="images/collections/earrings/crystal/01-03-03-P1100464.JPG" class="thumbnail" title="item: e-058">
